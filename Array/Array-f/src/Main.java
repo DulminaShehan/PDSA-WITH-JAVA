02e8f63512d2e8c1cfbdfc03e535c35457cce9fa
Dulminashehan <dulminashehan596@gmail.com>
2026-01-13 08:21:01 +0530

    Update Main.java

--- Array/Array-f/src/Main.java
+++ Array/Array-f/src/Main.java
@@ -1,10 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-int [] arr={40,5,20,1,95,100,27};
-int n= arr.length;
+ int [] arr ={10,20,30,40,50};
+    System.out.println(arr[4]+arr[1]);
 
-for(int i=0;i<n;i++){
-    System.out.println("Number is = " + arr[i] );;
- }
+    char []name = {'A','B','C','D'};
+    System.out.println(name[0] );
+    System.out.println(arr.length);
+
+
+}
+static void q1(int i){
+   
 }
