5546789226e1495b4489e913f600534501b3d4af
Dulminashehan <dulminashehan596@gmail.com>
2026-01-14 11:56:19 +0530

    Update Main.java

--- Array/Array-f/src/Main.java
+++ Array/Array-f/src/Main.java
@@ -8,8 +8,17 @@ void main() {
     System.out.println(name[0] );
     System.out.println(arr.length);
 
-
+    q1(0);
 }
 static void q1(int i){
-   
+    System.out.println("---------------------------------");
+    int[] arr1 = {10, 20, 30, 40, 50};
+    arr1[0] = i;  // update first element
+
+    for(int j = 0; j < arr1.length; j++){
+        System.out.println(arr1[j]);
+    }
+}
+static void q2(int i){
+    
 }
