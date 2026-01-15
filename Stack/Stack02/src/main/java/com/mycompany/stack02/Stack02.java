2bac59690237e3f08f6789a6257f9ad45efef141
Dulminashehan <dulminashehan596@gmail.com>
2026-01-15 11:32:38 +0530

    add

---
+++ Stack/Stack02/src/main/java/com/mycompany/stack02/Stack02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stack02;
+
+/**
+ *
+ * @author LOQ
+ */
+public class Stack02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Stack/Stack1/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+     class Stack{
+         int arr[] ;
+         int capacity;
+         int top;
+
+         Stack(int size){
+             arr= new int[size];
+             capacity =size;
+             top=-1;
+         }
+     }
+
+
+
+
+}
