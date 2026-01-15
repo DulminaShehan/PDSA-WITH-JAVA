f133e960508dfe079975e16e5e48c3af76711de7
Dulminashehan <dulminashehan596@gmail.com>
2026-01-15 17:36:05 +0530

    Stack-push

--- Stack/Stack02/src/main/java/com/mycompany/stack02/Stack02.java
+++ Stack/Stack02/src/main/java/com/mycompany/stack02/Stack02.java
@@ -9,8 +9,34 @@ package com.mycompany.stack02;
  * @author LOQ
  */
 public class Stack02 {
-
+     int arr[];
+     int top;
+     int capacity;
+     Stack02(int size){
+     arr=new int[size];
+     capacity=size;
+     top=-1;
+     
+     }
+     boolean isfull(){
+     return top+1==capacity;
+     
+     }
+     void push(int x){
+      if(isfull()){
+          System.out.println("Over Flow");
+            
+      }
+      else 
+      {
+          top++;
+          arr[top]=x;
+          System.out.println("insert in"+ x);
+      }
+      
+     }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Stack02 mystack= new Stack02(5);
+        mystack.push(10);
     }
 }
