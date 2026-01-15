d57d2cdc21b52fb152790f1b5714580f71f3c74f
Dulminashehan <dulminashehan596@gmail.com>
2026-01-16 02:14:28 +0530

    isEmpty,isFull,print

--- Stack/Stack02/src/main/java/com/mycompany/stack02/Stack02.java
+++ Stack/Stack02/src/main/java/com/mycompany/stack02/Stack02.java
@@ -22,6 +22,16 @@ public class Stack02 {
      return top+1==capacity;
      
      }
+    boolean isEmpty(){
+     return top==-1;
+     }
+    void printstack(){
+        System.out.println("Printing stack ");
+     for(int i=0;i<=top;i++){
+         System.out.println(arr[i]);
+     }
+    
+    }
      void push(int x){
       if(isfull()){
           System.out.println("Over Flow");
@@ -31,12 +41,35 @@ public class Stack02 {
       {
           top++;
           arr[top]=x;
-          System.out.println("insert in"+ x);
+          System.out.println("insert in ="+ x);
       }
       
+     }
+     void pop(){
+     if(isEmpty()){
+         System.out.println("Stack Empty");
+     }
+     else{
+     
+         System.out.println("Removing ="+arr[top]);
+         top--;
+     }
+     
+     }
+     void size(){
+         System.out.println(top+1);
      }
     public static void main(String[] args) {
         Stack02 mystack= new Stack02(5);
         mystack.push(10);
+        mystack.push(20);
+        mystack.push(30);
+        mystack.push(40);
+        mystack.push(50);
+        mystack.pop();
+        mystack.pop();
+        mystack.printstack();
+        mystack.size();
+     
     }
 }
