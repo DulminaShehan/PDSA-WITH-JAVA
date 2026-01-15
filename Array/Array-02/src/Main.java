d1fb37c7cd1a82c0a9bf3e7d44f5dd26549731bb
Dulminashehan <dulminashehan596@gmail.com>
2026-01-15 10:20:44 +0530

    Update Main.java

--- Array/Array-02/src/Main.java
+++ Array/Array-02/src/Main.java
@@ -5,41 +5,53 @@ import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run
 void main() {
 // This Use insert, Delete, Search, Update Array
 
-    int[] arr= new int[10];// array length
-    int size=5;
+    int[] arr = new int[10]; // array length
+    int size = 5;
 
-    Scanner Userin=new Scanner(System.in);
+    Scanner Userin = new Scanner(System.in);
 
     System.out.println("Enter Data :");
-    for(int j=0;j<size;j++){
-        arr[j]=Userin.nextInt();
+    for (int j = 0; j < size; j++) {
+        arr[j] = Userin.nextInt();
     }
+
     System.out.println("---------------------------------------------------------");
     System.out.println("Output of Array ");
-    for(int i=0;i<size;i++){
-        System.out.println(arr[i]);;
+    for (int i = 0; i < size; i++) {
+        System.out.println(arr[i]);
     }
+
+    // -------- Insert --------
     System.out.println("---------------------------------------------------------");
-    //insert - Start
-    System.out.println("Enter you index =");
-    int ind=Userin.nextInt();
+    System.out.println("Enter your index =");
+    int insertIndex = Userin.nextInt();
 
     System.out.println("Enter the Element ");
-    int ele=Userin.nextInt();
+    int ele = Userin.nextInt();
 
+    for (int k = size; k > insertIndex; k--) {
+        arr[k] = arr[k - 1];
+    }
+    arr[insertIndex] = ele;
     size++;
 
-    for (int k=size;k>ind;k--){
-        arr[k]=arr[k-1];
+    System.out.println("Output of Array after Insert:");
+    for (int i = 0; i < size; i++) {
+        System.out.println(arr[i]);
+    }
 
-    }
-    arr[ind]=ele;
-    System.out.println("Output of Array");
-    for(int i=0;i<size;i++){
-        System.out.println(arr[i]);;
-    }
-    //Insert - End
+    // -------- Delete --------
     System.out.println("---------------------------------------------------------");
-    //Delete
-    System.out.println("Enter you Delete");
+    System.out.println("Enter your Delete index =");
+    int deleteIndex = Userin.nextInt();
+
+    for (int j = deleteIndex; j < size - 1; j++) {
+        arr[j] = arr[j + 1];
+    }
+    size--;
+
+    System.out.println("Output of Array after Delete:");
+    for (int i = 0; i < size; i++) {
+        System.out.println(arr[i]);
+    }
 }
