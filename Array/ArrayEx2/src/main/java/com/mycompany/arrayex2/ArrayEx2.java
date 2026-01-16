9af2dec53e8b8212a546ecb7e1da182f5a405ddd
Dulminashehan <dulminashehan596@gmail.com>
2026-01-17 03:11:13 +0530

    ex2

---
+++ Array/ArrayEx2/src/main/java/com/mycompany/arrayex2/ArrayEx2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arrayex2;
+
+/**
+ *
+ * @author LOQ
+ */
+public class ArrayEx2 {
+
+    public static void main(String[] args) {
+        int[]numbers={10,20,30,40,50,};
+        int sum=0;
+        
+        for(int i=0;i<numbers.length;i++){
+         sum= sum + numbers[i];
+         
+        }
+        System.out.println("Sum of array values = "+ sum);
+        
+    }
+}
